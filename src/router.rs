a5bb4684ebfde0c6d23a632c905337e2c163d1b4
Jeancarlo <jeancarlobarrios@gmail.com>
2024-06-26 14:39:57 -0600

    feat: add static server

--- src/router.rs
+++ src/router.rs
@@ -1,7 +1,10 @@
 use axum::{routing::get, Router};
+use tower_http::services::ServeDir;
 
 use crate::routes;
 
 pub fn init_router() -> Router {
-    Router::new().route("/", get(routes::home))
+    Router::new()
+        .route("/", get(routes::home))
+        .nest_service("/static", ServeDir::new("static"))
 }

22edcd0f0e51e4fdb6acbafff08405b97746ab47
Pablo <pjestradac@gmail.com>
2024-07-30 14:51:05 -0600

    wip: create datastore gcs and search chunks

--- crates/vertex_ai/src/data_store/data_store.rs
+++ crates/vertex_ai/src/data_store/data_store.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 use crate::client::Client;
-
+use tokio::time::{sleep, Duration};
 const BASE_SCOPE: &str = "https://www.googleapis.com/auth/cloud-platform";
 
 pub struct DataStoreClient {
@@ -71,6 +71,36 @@ impl DataStoreClient {
             .map_err(Error::HttpStatus)?;
 
         let operation: Operation = response.json().await.map_err(Error::ResponseJsonParsing)?;
+
+        Ok(operation)
+    }
+
+
+    // Sets up a Google cloud storage data store
+    pub async fn setup_data_connector(
+        &self,
+        request: SetupDataConnectorRequest,
+    ) -> Result<SetupDataConnectorResponse, Error> {
+        let location = "global";
+
+
+        let url = reqwest::Url::parse(
+            format!(
+                "https://discoveryengine.googleapis.com/v1/projects/{}/locations/{}/global:setUpDataConnector",
+                request.project_id, location,
+            )
+                .as_str(),
+        );
+
+        let response = self
+            .client
+            .api_post(&[BASE_SCOPE], url.unwrap().as_str(), request)
+            .await
+            .map_err(Error::ClientError)?
+            .error_for_status()
+            .map_err(Error::HttpStatus)?;
+
+        let operation: SetupDataConnectorResponse = response.json().await.map_err(Error::ResponseJsonParsing)?;
 
         Ok(operation)
     }
@@ -260,6 +290,134 @@ impl DataStoreClient {
             response.json().await.map_err(Error::ResponseJsonParsing)?;
         Ok(list_chunks_response)
     }
+
+
+    pub async fn get_operation_status(&self, request: GetOperationStatusRequest) -> Result<Operation, Box<dyn std::error::Error>> {
+        let url = format!("https://discoveryengine.googleapis.com/v1alpha/{}", request.name);
+        let response = self
+            .client
+            .api_get(&[BASE_SCOPE], &url)
+            .await
+            .map_err(Error::ClientError)?
+            .error_for_status()
+            .map_err(Error::HttpStatus)?;
+
+        if response.status().is_success() {
+            let operation: Operation = response.json().await?;
+            Ok(operation)
+        } else {
+            let error = response.text().await?;
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, error)))
+        }
+    }
+
+    pub async fn poll_operation(&self, operation_name: &str, max_retries: Option<u32>, wait_time: Option<u64>) -> bool {
+        let max_retries = max_retries.unwrap_or(20);
+        let wait_time = wait_time.unwrap_or(5);
+        let mut retries = 0;
+
+        loop {
+            let request = GetOperationStatusRequest {
+                name: operation_name.to_string(),
+            };
+            let operation_status = self.get_operation_status(request).await;
+            match operation_status {
+                Ok(status) if status.done => return true,
+                Ok(_) => {
+                    retries += 1;
+                    if retries >= max_retries {
+                        return false;
+                    }
+                    sleep(Duration::from_secs(wait_time)).await; // Adjust the duration as needed
+                }
+                Err(_) => return false,
+            }
+        }
+    }
+    pub async fn search_chunks(
+        &self,
+        request: SearchChunksRequest,
+    ) -> Result<SearchChunksResponse, Error> {
+        let location = "global";
+
+        let url = format!(
+            "https://discoveryengine.googleapis.com/v1alpha/projects/{}/locations/{}/collections/{}/dataStores/{}/servingConfigs/default_search:search",
+            request.project_id, location, request.collections, request.data_store_id
+        );
+        let response = self
+            .client
+            .api_get_with_params(&[BASE_SCOPE], &url, None)
+            .await
+            .map_err(Error::ClientError)?
+            .error_for_status()
+            .map_err(Error::HttpStatus)?;
+        let search_chunks_response: SearchChunksResponse =
+            response.json().await.map_err(Error::ResponseJsonParsing)?;
+        Ok(search_chunks_response)
+    }
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SetupDataConnectorResponse {
+    pub name: String,
+    pub response: ResponseDataConnector,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ResponseDataConnector {
+    #[serde(rename = "@type")]
+    pub type_url: String,
+    pub name: String,
+    pub state: String,
+    pub data_source: String,
+    pub params: Params,
+    pub refresh_interval: String,
+    pub entities: Vec<ResponseEntity>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ResponseEntity {
+    pub entity_name: String,
+    pub data_store: String,
+    pub params: EntityParams,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct  SetupDataConnectorRequest {
+    pub project_id: String,
+    pub collection_id: String,
+    pub collection_display_name: String,
+    pub data_connector: DataConnector,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DataConnector {
+    pub data_source: String,
+    pub params: Params,
+    pub refresh_interval: String,
+    pub entities: Vec<Entity>,
+    pub sync_mode: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Params {
+    pub instance_uris: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Entity {
+    pub entity_name: String,
+    pub params: EntityParams,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EntityParams {
+    pub data_schema: String,
+    pub content_config: String,
+    pub industry_vertical: String,
+    pub auto_generate_ids: bool,
 }
 
 pub struct ListChunksRequest {
@@ -278,6 +436,43 @@ pub struct ListChunksResponse {
     pub next_page_token: Option<String>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ChunkSpec {
+    pub num_previous_chunks: Option<i32>,
+    pub num_next_chunks: Option<i32>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContentSearchSpec {
+    pub chunk_spec: Option<ChunkSpec>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SearchChunksRequest {
+    pub project_id: String,
+    pub collections: String,
+    pub data_store_id: String,
+    pub serving_config: String,
+    pub query: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub page_size: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub page_token: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub offset: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub filter: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub order_by: Option<String>,
+    pub content_search_spec: ContentSearchSpec
+
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SearchChunksResponse {
+    pub chunks: Vec<Chunk>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub next_page_token: Option<String>,
+}
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Chunk {
     pub name: String,
@@ -341,12 +536,24 @@ pub struct CreateDataStoreRequest {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct GetOperationStatusRequest {
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Operation {
     pub name: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Metadata>,
     pub done: bool,
     pub response: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct OperationError {
+    pub code: i32,
+    pub message: String,
+    pub details: Vec<HashMap<String, serde_json::Value>>, // Adjust as needed
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -501,6 +708,113 @@ mod tests_integrations {
     // Test create_data_store
     #[tokio::test]
     async fn test_create_data_store() {
+        env::set_var(
+            "GOOGLE_APPLICATION_CREDENTIALS",
+            "../../private/gcp_key.json",
+        );
+
+        let project_id = "moni-429523";
+        let collections = "default_collection";
+        let data_store = DataStore {
+            name: "moni-test".to_string(),
+            display_name: "moni-test".to_string(),
+            industry_vertical: IndustryVertical::Generic,
+            solution_types: vec![],
+            default_schema_id: None,
+            content_config: ContentConfig::PublicWebsite,
+            create_time: None,
+            language_info: None,
+            document_processing_config: None,
+            starting_schema: None,
+        };
+
+        let data_store_id = "test-1";
+
+        let data_store_request = CreateDataStoreRequest {
+            data_store,
+            project_id: project_id.to_string(),
+            collections: collections.to_string(),
+            data_store_id: data_store_id.to_string(),
+            create_advance_site_search: None,
+        };
+
+        let client = DataStoreClient::new().await.unwrap();
+        // let operation = client.create_data_store(data_store_request).await;
+
+        /* println!("{:?}", operation);
+
+        assert!(operation.is_ok());
+
+        println!("{:?}", operation.unwrap()); */
+
+        // Now lets delete it
+        let delete_request = DeleteDataStoreRequest {
+            project_id: project_id.to_string(),
+            collections: collections.to_string(),
+            data_store_id: data_store_id.to_string(),
+        };
+        let delete_operation = client.delete_data_store(delete_request).await;
+
+        println!("{:?}", delete_operation);
+        assert!(delete_operation.is_ok());
+        println!("{:?}", delete_operation.unwrap());
+    }
+
+    // Test create_data_store with a storage bucket.
+    #[tokio::test]
+    async fn test_create_data_store_storage_bucket() {
+        env::set_var(
+            "GOOGLE_APPLICATION_CREDENTIALS",
+            "../../private/gcp_key.json",
+        );
+
+        let project_id = "moni-429523";
+        let collections = "default_collection";
+        let data_store_id = "test-1-gcp";
+
+        let data_store_request = SetupDataConnectorRequest {
+            project_id: project_id.to_string(),
+            collection_id: "moni-demo-1_1722304644136".to_string(),
+            collection_display_name: "moni-demo-1-gcs".to_string(),
+            data_connector: DataConnector {
+                data_source: "gcs".to_string(),
+                params: Params {
+                    instance_uris: vec!["gs://moni-demo-1".to_string()],
+                },
+                refresh_interval: "86400s".to_string(),
+                entities: vec![Entity {
+                    entity_name: "gcs_store".to_string(),
+                    params: EntityParams {
+                        data_schema: "content-with-faq-csv".to_string(),
+                        content_config: "content_required".to_string(),
+                        industry_vertical: "industry_vertical_unspecified".to_string(),
+                        auto_generate_ids: false,
+                    },
+                }],
+                sync_mode: "PERIODIC".to_string(),
+            },
+
+        };
+
+        let setup_connector_response = DataStoreClient::new().await.unwrap().setup_data_connector(data_store_request).await;
+
+        let client = DataStoreClient::new().await.unwrap();
+
+        // Now lets delete it
+        let delete_request = DeleteDataStoreRequest {
+            project_id: project_id.to_string(),
+            collections: collections.to_string(),
+            data_store_id: data_store_id.to_string(),
+        };
+        let delete_operation = client.delete_data_store(delete_request).await;
+
+        println!("{:?}", delete_operation);
+        assert!(delete_operation.is_ok());
+        println!("{:?}", delete_operation.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_search_data_store() {
         env::set_var(
             "GOOGLE_APPLICATION_CREDENTIALS",
             "../../private/gcp_key.json",
